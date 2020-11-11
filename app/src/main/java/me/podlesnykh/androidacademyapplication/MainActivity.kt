0af48ab5005860ddc1ad628c74995c52caabf6ce
desttruptor <41233120+desttruptor@users.noreply.github.com>
2020-11-11 21:48:15 +0300

    ui fixes

--- app/src/main/java/me/podlesnykh/androidacademyapplication/MainActivity.kt
+++ app/src/main/java/me/podlesnykh/androidacademyapplication/MainActivity.kt
@@ -7,9 +7,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val intent = Intent(this, MovieDetailsActivity::class.java)
-        startActivity(intent)
+        setContentView(R.layout.activity_movie_details)
     }
 }
