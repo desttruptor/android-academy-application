c37551593d21aa20ec32924381c35cbec68cfa48
desttruptor <41233120+desttruptor@users.noreply.github.com>
2020-11-15 16:53:47 +0300

    some fragment stuff

---
+++ app/src/main/java/me/podlesnykh/androidacademyapplication/FragmentMovieDetails.kt
@@ -0,0 +1,27 @@
+package me.podlesnykh.androidacademyapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class FragmentMovieDetails : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.activity_movie_details, container, false)
+
+        // textView is used as a button
+        val tvBack: TextView = rootView.findViewById(R.id.tv_back)
+        tvBack.setOnClickListener{
+            activity
+                ?.supportFragmentManager
+                ?.beginTransaction()
+                ?.remove(this)
+                ?.commit()
+        }
+
+        return rootView
+    }
+}
