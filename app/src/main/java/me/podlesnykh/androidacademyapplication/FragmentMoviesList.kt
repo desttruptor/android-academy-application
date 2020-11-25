412d15da96fbd94751548bce221468892943c483
desttruptor <41233120+desttruptor@users.noreply.github.com>
2020-11-26 02:00:24 +0300

    added navigation

--- app/src/main/java/me/podlesnykh/androidacademyapplication/FragmentMoviesList.kt
+++ app/src/main/java/me/podlesnykh/androidacademyapplication/FragmentMoviesList.kt
@@ -4,27 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 
-class FragmentMovieDetails : Fragment() {
+class FragmentMoviesList : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_movie_details, container, false)
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
 
-        // textView is used as a button
-        val tvBack: TextView = view.findViewById(R.id.tv_back)
-        tvBack.setOnClickListener {
+        val card: View = view.findViewById(R.id.movie_item_container)
+        card.setOnClickListener {
             val navHostFragment = requireActivity()
                 .supportFragmentManager
                 .findFragmentById(R.id.nav_host_fragment)
                     as NavHostFragment
             val navController = navHostFragment.navController
-            navController.navigate(R.id.navigate_to_movies_list)
+            navController.navigate(R.id.navigate_to_movie_item)
         }
 
         return view
--- app/src/main/java/me/podlesnykh/androidacademyapplication/MainActivity.kt
+++ app/src/main/java/me/podlesnykh/androidacademyapplication/MainActivity.kt
@@ -1,19 +1,11 @@
 package me.podlesnykh.androidacademyapplication
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fragmentMovieDetails = FragmentMovieDetails()
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.main_activity_root, fragmentMovieDetails)
-        transaction.addToBackStack(null)
-        transaction.commit()
     }
 }
